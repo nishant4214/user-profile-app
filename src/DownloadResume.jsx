a56f8d312a490b4d6c9b2f4954bd01144d182f75
Nishant.Pande <Nishant.Pande@ESDS003568NP>
2024-10-16 13:11:30 +0530

    file changes

--- src/DownloadResume.jsx
+++ src/DownloadResume.jsx
@@ -1,52 +1,38 @@
 import React, { useState } from 'react';
 import FileDownloadDoneRoundedIcon from '@mui/icons-material/FileDownloadDoneRounded';
 import DownloadIcon from '@mui/icons-material/Download';
-import {
-    Paper,
-    Box,Typography
-  } from '@mui/material';
+import { Paper, Box, Typography } from '@mui/material';
 
 function DownloadResumeIcon() {
     const [isDownloaded, setIsDownloaded] = useState(false);
     const resumeUrl = '/sample.pdf'; // Ensure the leading slash for the public folder
 
-    const handleDownload = (e) => {
-        // Prevent default anchor behavior
-        e.preventDefault();
-
-        // Start the download
+    const handleDownload = () => {
         const link = document.createElement('a');
         link.href = resumeUrl;
-        link.download = 'sample.pdf'; // Optional: specify the file name
+        link.setAttribute('download', 'sample.pdf'); // Specify the file name
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
-
-        // Indicate that the download has started
         setIsDownloaded(true);
-
-        // Reset the download state after a few seconds
-        setTimeout(() => {
-            setIsDownloaded(false);
-        }, 3000); // Reset after 3 seconds
+        setTimeout(() => setIsDownloaded(false), 3000);
     };
 
-    return (<>
+    return (
         <Paper elevation={3} style={{ padding: '20px' }}>
             <Typography color='blue' variant="h4" align="left" gutterBottom>
                 Resume
             </Typography>
-            <Box display="flex" flexDirection="column" alignItems="center">                 
-                <a 
-                    href={resumeUrl} 
+            <Box display="flex" flexDirection="column" alignItems="center">
+                <button 
                     onClick={handleDownload} 
-                    style={{ cursor: 'pointer', textDecoration: 'none' }}
+                    style={{ cursor: 'pointer', border: 'none', background: 'none' }}
                     aria-label="Download Resume"
                 >
-                    {isDownloaded ? <FileDownloadDoneRoundedIcon sx={{ fontSize: 40 }}/> : <DownloadIcon sx={{ fontSize: 40 }}/>}
-                </a>
+                    {isDownloaded ? <FileDownloadDoneRoundedIcon sx={{ fontSize: 40 }} /> : <DownloadIcon sx={{ fontSize: 40 }} />}
+                </button>
             </Box>
-        </Paper></>
+        </Paper>
     );
 }
 
