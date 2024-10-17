dfae9eca2eda2909add2d5c92d9f1d724696429f
Nishant.Pande <Nishant.Pande@ESDS003568NP>
2024-10-17 15:34:43 +0530

    icon added

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import UserProfile from './UserProfile'
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,4 +11,3 @@ root.render(
   </React.StrictMode>
 );
 
-reportWebVitals();
