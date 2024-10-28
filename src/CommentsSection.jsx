1221e7f27d4152e7203fe711c150c9a258eeca4f
Nishant.Pande <Nishant.Pande@ESDS003568NP>
2024-10-28 15:30:08 +0530

    instagram logo removed

--- src/CommentsSection.jsx
+++ src/CommentsSection.jsx
@@ -1,116 +1,145 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
-import './CommentsSection.css'
-import { Paper, Box, Typography } from '@mui/material';
+import './CommentsSection.css';
+import { Paper, Box, Typography, TextField, CircularProgress, Button, Snackbar } from '@mui/material';
 
 const CommentsSection = ({ projectId }) => {
-  const [comments, setComments] = useState([]);
+
+  
+
   const [commentText, setCommentText] = useState('');
   const [emailId, setEmailId] = useState('');
   const [userName, setUserName] = useState('');
   const [userMobileNumber, setUserMobileNumber] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [open, setOpen] = React.useState(false);
+  const [snackText, setSnackText] = useState('');
 
-  // const fetchComments = useCallback(async () => {
-  //   try {
-  //     const response = await axios.get(`https://profile-api-nishant.netlify.app/.netlify/functions/Comments?projectId=${projectId}`);
-  //     setComments(response.data);
-  //   } catch (error) {
-  //     console.error('Error fetching comments:', error);
-  //   }
-  // }, [projectId]);
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
 
-  // useEffect(() => {
-  //   // Fetch comments when the component mounts
-  
-  //   fetchComments();
-  // }, [fetchComments]);
+    setOpen(false);
+  };
   const handleCommentSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true);
 
-    const newComment = { projectId, commentText:commentText,emailId:emailId,userName:userName,userMobileNumber:userMobileNumber };
-    try 
-    {
-        const response = await fetch('https://profile-api-nishant.netlify.app/.netlify/functions/setComment', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(newComment),
-        });
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        const data = await response.json();
-        setComments([...comments, data.comment]); // Update state
-        setUserName(''); // Clear input
-        setEmailId(''); // Clear input
-        setUserMobileNumber(''); // Clear input
-        setCommentText(''); // Clear input
-        // fetchComments(); // Fetch updated comments after submitting
+    // Basic client-side validation
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailPattern.test(emailId)) {
+     // show('Please enter a valid email address', 'error');
+      setLoading(false);
+      return;
+    }
+
+    const newComment = {
+      projectId,
+      commentText,
+      emailId,
+      userName,
+      userMobileNumber,
+    };
+
+    try {
+      const response = await fetch('https://profile-api-nishant.netlify.app/.netlify/functions/setComment', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newComment),
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      setUserName('');
+      setEmailId('');
+      setUserMobileNumber('');
+      setCommentText('');
+      setSnackText('We will get back to you shortly.')
+      setOpen(true);
 
     } catch (error) {
-        console.error('Error submitting comment:', error);
-        alert('Failed to submit comment. Please try again.');
+      console.error('Error submitting comment:', error); // Log error for debugging
+      setSnackText('Failed to send enquiry. Please try again.')
+      setOpen(true);
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
-    <Paper 
-            style={{
-            backgroundColor: 'transparent',
-            padding: '20px'
-            }}
-            elevation={2}>
+    <Paper style={{ backgroundColor: 'transparent', padding: '20px' }} elevation={2}>
       <Typography color='blue' variant="h4" align="left" gutterBottom>
         Enquiry
       </Typography>
-        <Box sx={{ overflowX: 'scroll' }}>
-
-            <form className="comment-form" onSubmit={handleCommentSubmit}>
-              <input
-                value={userName}
-                onChange={(e) => setUserName(e.target.value)}
-                placeholder="Enter your name..."
-                required
-              />
-              <br/>
-              <input
-                className='input'
-                value={emailId}
-                onChange={(e) => setEmailId(e.target.value)}
-                placeholder="Enter your email id..."
-                type='email'
-                required
-              />
-              <br/>
-              <input
-                value={userMobileNumber}
-                onChange={(e) => setUserMobileNumber(e.target.value)}
-                placeholder="Enter your mobile number..."
-                maxLength={15}
-                required
-              />
-              <br/>
-              <textarea
-                value={commentText}
-                onChange={(e) => setCommentText(e.target.value)}
-                placeholder="Leave a comment"
-                required
-              />
-              <button type="submit">Submit</button>
-            </form>
-            <ul className="comments-list">
-            {comments
-              .slice() // Create a shallow copy of the comments array
-              .reverse() // Reverse the order of the comments
-              .map((comment, index) => (
-                <li key={index}>
-                  <strong>{comment.users?.username || 'Unknown User'}</strong>: {comment.comment_text || comment}
-                </li>
-              ))}
-          </ul>
+      <div>
+      <Snackbar
+          open={open}
+          autoHideDuration={5000}
+          onClose={handleClose}
+          message={snackText}
+        />
+      </div>
+      <Box sx={{ overflowX: 'scroll' }}>
+        <form className="comment-form" onSubmit={handleCommentSubmit}>
+          <TextField
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+            placeholder="Enter your name..."
+            required
+            fullWidth
+            variant="outlined"
+            margin="normal"
+            label="Your Name"
+          />
+          <br />
+          <TextField
+            className="input"
+            value={emailId}
+            onChange={(e) => setEmailId(e.target.value)}
+            placeholder="Enter your email id..."
+            type="email"
+            required
+            fullWidth
+            variant="outlined"
+            margin="normal"
+            label="Email ID"
+          />
+          <br />
+          <TextField
+            value={userMobileNumber}
+            onChange={(e) => setUserMobileNumber(e.target.value)}
+            placeholder="Enter your mobile number..."
+            required
+            fullWidth
+            variant="outlined"
+            margin="normal"
+            label="Mobile Number"
+            inputProps={{ maxLength: 15 }}
+          />
+          <br />
+          <TextField
+            value={commentText}
+            onChange={(e) => setCommentText(e.target.value)}
+            placeholder="Enter details..."
+            required
+            fullWidth
+            variant="outlined"
+            margin="normal"
+            label="Details"
+            multiline
+            rows={2}
+          />
+          <Button type="submit" variant="contained" color="primary" disabled={loading}>
+            {loading ? <CircularProgress size={24} /> : 'Submit'}
+          </Button>
+        </form>
       </Box>
-  </Paper>
+    </Paper>
   );
 };
 
